89f8fb08cc437f6122fe6303b72f9eff485146dd
jason.ks.chiu <jason.ks.chiu@deltaww.com>
2025-07-03 16:34:13 +0800

    feat: 新增 Token 更新功能的 Web Worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useEffect, useContext } from "react";
 import AppRouter from './routers/AppRouter'
 import { AuthProvider } from "./contexts/AuthContext";
 import { GlobalProvider } from "./contexts/GlobalContext";
-const App = () => (
-  <AuthProvider >
-    <GlobalProvider>
-      <AppRouter />
-    </GlobalProvider>
-  </AuthProvider>
-)
+import refreshTokenWorkerImport from './worker/refreshTokenWorker';
+
+const App = () => {
+
+  useEffect(() => {
+    if (localStorage.getItem("keep") == 1) {
+      const refreshTokenWorker = new Worker(refreshTokenWorkerImport);
+      refreshTokenWorker.postMessage({ type: 'start', jwt: token });
+
+      refreshTokenWorker.onmessage = (e) => {
+        const { type } = e.data;
+
+        if (type === 'refreshToken') {
+          // 重新整理 Token
+          renewToken()
+            .then((token) => {
+              refreshTokenWorker.postMessage({
+                type: 'start', jwt:
+                  token
+              });
+
+              console.log("refresh token success");
+            });
+        }
+        else if (type === 'getToke') {
+          console.log("get token");
+          refreshTokenWorker.postMessage({ type: 'start', jwt: token });
+        }
+      };
+
+      return () => {
+        refreshTokenWorker.terminate();
+      };
+    }
+  }, []);
+
+  return (
+    <AuthProvider >
+      <GlobalProvider>
+        <AppRouter />
+      </GlobalProvider>
+    </AuthProvider>
+  )
+};
 
 export default App
---
+++ src/worker/refreshTokenWorker.js
@@ -0,0 +1,71 @@
+
+const webWorker = () => {
+    let jwt = '';
+    let checkInterval = null;
+
+    onmessage = async (e) => {
+        const { type, jwt: newJwt } = e.data;
+
+        if (type === 'start') {
+            console.log("worker: start");
+            jwt = newJwt;
+
+            if (checkInterval) {
+                clearInterval(checkInterval);
+            }
+
+            checkInterval = setInterval(async () => {
+                await checkTokenExpiration();
+            }, 300000); // 5 分鐘檢查一次
+
+            await checkTokenExpiration();
+        }
+        else if (type === 'refreshToken') {
+            clearInterval(checkInterval);
+        }
+    };
+
+    async function checkTokenExpiration() {
+        if (!jwt) {
+            console.log("worker: no token, get token");
+            postMessage({ type: 'getToken' });
+            setTimeout(() => {
+                checkTokenExpiration();
+            }, 60000);
+            return;
+        }
+
+        const decoded = decodeJwt(jwt);
+        const expTime = decoded.exp * 1000;
+        const currentTime = Date.now();
+
+        console.log('Checking token expiration...');
+
+        if (currentTime >= expTime) {
+            console.log('Token has expired, clearing token...');
+            jwt = '';
+            postMessage({ type: 'getToken' });
+            return;
+        }
+
+        const nearExpiry = expTime - currentTime <= 600000;
+
+        if (nearExpiry) {
+            console.log('Token is about to expire, refreshing...');
+            postMessage({ type: 'refreshToken' });
+        }
+    }
+
+    function decodeJwt(token) {
+        const payload = token.split('.')[1];
+        const decoded = atob(payload);
+        return JSON.parse(decoded);
+    }
+}
+
+
+let code = webWorker.toString()
+code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"))
+const blob = new Blob([code], { type: 'application/javascripts' })
+const workerScript = URL.createObjectURL(blob)
+module.exports = workerScript;
